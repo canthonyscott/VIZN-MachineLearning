d01e8b80d56b98794aa839486732318fe7a79375
Anthony Scott <ascott329@gmail.com>
2016-11-01 19:00:44 -0500

    switch to class views

--- analysis/urls.py
+++ analysis/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url, include
 
-from . import views
+from analysis.views import IndexView
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', IndexView.as_view()),
 
 ]
--- analysis/views.py
+++ analysis/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
 
 # Create your views here.
 
 
-def index(request):
-    return render(request, 'analysis/index.html')
+class IndexView(View):
+
+    def get(self, request):
+        return render(request, 'analysis/index.html')
+
