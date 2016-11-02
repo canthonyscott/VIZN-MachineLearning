9297aaeef0f15a5014f9660e2977127cb47707f8
Anthony Scott <ascott329@gmail.com>
2016-11-02 14:37:09 -0500

    session login working

--- login/urls.py
+++ login/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url, include
 from login.views import IndexView
 
 urlpatterns = [
-    url(r'^$', IndexView.as_view()),
+    url(r'^$', IndexView.as_view(), name='login'),
 ]
--- login/views.py
+++ login/views.py
@@ -1,11 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+from .forms import LoginForm
+from django.contrib.auth import authenticate, login
+from django.urls import reverse
+from django.http import HttpResponse
+
 
 # Create your views here.
 
 
 class IndexView(View):
 
+    form = LoginForm()
+
     def get(self, request):
-        return render(request, 'login/index.html')
+        return render(request, 'login/index.html', {'form': self.form})
+
+    def post(self, request):
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            #todo something to login user
+            login(request, user)
+            return redirect('/analysis/')
+        else:
+            return render(request, 'login/index.html', {'form': self.form, 'error':'Credentials are invalid'})
+
+
 
