9619cac476f27fa2c8577e6ecdd5586bc0623f34
Anthony Scott <ascott329@gmail.com>
2016-11-03 20:24:09 -0500

    root redirect to login

--- vizn_ml_django/urls.py
+++ vizn_ml_django/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.http import HttpResponseRedirect
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^analysis/', include('analysis.urls')),
-    url(r'^login/', include('login.urls'))
+    url(r'^login/', include('login.urls')),
+    url(r'^$', lambda r: HttpResponseRedirect('/login/')),
 ]
