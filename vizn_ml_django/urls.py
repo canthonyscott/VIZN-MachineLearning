ac39331210d2bd26c871b9230fb93cf49cb631c3
Anthony Scott <ascott329@gmail.com>
2016-11-03 20:38:20 -0500

    logout button added, nav bar

--- vizn_ml_django/urls.py
+++ vizn_ml_django/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.http import HttpResponseRedirect
+from django.contrib.auth import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^analysis/', include('analysis.urls')),
     url(r'^login/', include('login.urls')),
+    url(r'^logout/', views.logout, {'next_page':'/login'}),
     url(r'^$', lambda r: HttpResponseRedirect('/login/')),
 ]
