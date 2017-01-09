bddb59de737d006d01efc527502b993e15407cd9
Anthony Scott <ascott329@gmail.com>
2017-01-09 10:59:46 -0600

    create method to no do my own normalization

--- analysis/scripts/preprocessor.py
+++ analysis/scripts/preprocessor.py
@@ -66,6 +66,64 @@ def one_file_extract_and_norm_timepoints(file):
     return output
 
 
+def one_file_extract_and_no_norm(file):
+    import pandas as pd
+    import numpy as np
+    from . import constants
+
+    flashes = [1800, 1830, 1860, 1890, 1920]
+    collection = []
+    wells_skipped = []
+
+    print("Extracting Stimuli time points...")
+
+    data = pd.read_table(file, low_memory=False)
+
+    if data.iloc[95,0] == 'w048':
+        plate_size = 48
+        fish_to_pull = constants.wells48
+        print("48 well plate detected, using 48 well layout.")
+
+    elif data.iloc[95,0] == 'w096':
+        fish_to_pull = constants.wells96
+        plate_size = 96
+        print("96 well plate detected, using 96 well layout.")
+
+    # pull out individual fish
+    for item in fish_to_pull:
+        fish = data.loc[data.animal == item].actinteg.values
+
+        if len(fish) < 1950:
+            wells_skipped.append(item)
+            continue
+
+        # calculate background activity level for normalization
+        baseline = fish[10:1979].mean()
+
+        # check for empty well/dead fish
+        if baseline == 0:
+            wells_skipped.append(item)
+            continue
+
+        # subset for desired times
+        timepoints = []
+        for flash in flashes:
+            timepoints.append(fish[flash])
+
+        # convert to nparray and normalize
+        # timepoints = np.array(timepoints)/baseline
+
+        collection.append(timepoints)
+
+    arr = np.array(collection)
+    print("Array generated from tab delim files")
+    print("n_samples, n_features: " + str(arr.shape))
+    # create dict of data to return
+    output = {'array': arr, 'skipped': wells_skipped, 'plate': plate_size}
+
+    return output
+
+
 def match_to_plate_new(result_array, skipped_wells, plate_size):
     # import pandas as pd
     # import numpy as np
