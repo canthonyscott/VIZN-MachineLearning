2a76128d7ffdaa44938234e9267fef1e7f4f0a39
Anthony Scott <ascott329@gmail.com>
2017-02-14 11:08:18 -0600

    added probability prediction from new model

--- analysis/scripts/NormalizedModelTesting.py
+++ analysis/scripts/NormalizedModelTesting.py
@@ -6,18 +6,20 @@ def testToModel_onefile(file):
     import os
 
     # load trained model
-    clf = joblib.load(os.path.join(BASE_DIR, 'analysis/scripts/model/new_model_Jan_31_2016.model'))
+    # clf = joblib.load(os.path.join(BASE_DIR, 'analysis/scripts/model/new_model_Jan_31_2016.model'))
+    clf = joblib.load(os.path.join(BASE_DIR, 'analysis/scripts/model/KNN_model_2_14_2017.pkl'))
     data = pc.one_file_extract_and_no_norm(file)
     test_data = data['array']
     test_data = pc.process_for_prediction(test_data)
 
     try:
         result = clf.predict(test_data)
+        result_prob = clf.predict_proba(test_data)
     except:
         print("DATA NOT AS EXPECTED. ABORTING.")
         exit(1)
 
-    output = {'result': result, 'skipped': data['skipped'], 'array': data['array'], 'plate': data['plate']}
+    output = {'result': result, 'result_prob': result_prob, 'skipped': data['skipped'], 'array': data['array'], 'plate': data['plate']}
 
     return output
 
