6895d087498ced7679c87d6d4062c72191ea2c15
Anthony Scott <ascott329@gmail.com>
2017-02-15 13:36:23 -0600

    ajax query working

--- analysis/static/analysis/scripts.js
+++ analysis/static/analysis/scripts.js
@@ -2,6 +2,8 @@
  * Created by anthony on 2/11/17.
  */
 
+var response;
+
 $(document).ready(function () {
     console.log("Ready");
     $body = $("body");
@@ -10,6 +12,18 @@ $(document).ready(function () {
     $('#upload_frm').submit(function () {
        console.log("form Submit");
         $body.addClass("loading");
+    });
+
+    $('#prob_btn').click(function () {
+        console.log('prob btn clicked');
+        var url = "/analysis/probabilities/?id=" + item_id;
+        console.log(url);
+        $.get(url, function (values) {
+            console.log(values)
+        });
+
+
+
     });
 
 });
